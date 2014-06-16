222f8c7c50684c1432937936e7312d4a49e5e81a
Charles Leifer <coleifer@gmail.com>
2014-06-16 13:03:23 -0500

    Holy crap...I think this finally works.

--- setup.py
+++ setup.py
@@ -6,22 +6,39 @@ import os
 import subprocess
 
 lib_vedis = Extension(
-    name='vedis.vedis',
+    name='vedis.libvedis',
     define_macros=[('VEDIS_ENABLE_THREADS', '1')],
     sources=['vedis/src/vedis.c'])
 
 class GenerateCtypesWrapper(build_py):
     def run(self):
+        cur_dir = os.path.realpath(os.path.dirname(__file__))
+        wrapper = os.path.join(cur_dir, 'vedis', '_vedis.py')
         subprocess.check_call([
-            'ctypesgen.py',
-            src('vedis.h'),
-            #'-L',
-            #'./',
+            os.path.join('ctypesgen', 'ctypesgen.py'),
+            os.path.join('vedis', 'src', 'vedis.h'),
+            '-L',
+            './',
             '-l',
             'vedis',
             '-o',
-            os.path.join(cur_dir, 'vedis', '_vedis.py')])
-        return super(GenerateCtypesWrapper, self).run()
+            wrapper])
+
+        # Read content of generated file.
+        with open(wrapper) as fh:
+            content = fh.readlines()
+
+        # Modify the add_library_path to use the current dir.
+        with open(wrapper, 'w') as fh:
+            for line in content:
+                if line.startswith('add_library_search_dirs('):
+                    fh.write('add_library_search_dirs(['
+                             'os.path.realpath(os.path.dirname(__file__))'
+                             '])\n')
+                else:
+                    fh.write(line)
+
+        return build_py.run(self)
 
 setup(
     name='vedis',
@@ -37,7 +54,6 @@ setup(
         ],
     },
     zip_safe=False,
-    install_requires=['ctypesgen==0.r125'],
     ext_modules=[lib_vedis],
     cmdclass={'build_py': GenerateCtypesWrapper},
 )
