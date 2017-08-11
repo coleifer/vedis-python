f0fefc67f489f921f25ed24a72aa77e4c4b78d52
Charles Leifer <coleifer@gmail.com>
2017-08-10 22:24:13 -0500

    0.5.0

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ vedis_extension = Extension(
 
 setup(
     name='vedis',
-    version='0.4.1',
+    version='0.5.0',
     description='Fast Python bindings for the Vedis embedded NoSQL database.',
     author='Charles Leifer',
     author_email='',
